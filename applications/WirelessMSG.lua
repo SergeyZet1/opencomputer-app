b12c6f7915c19ce433a9e7ca187c7c195e84e1f5
SergeyZet1 <pak432@yandex.ru>
2018-07-20 17:28:13 +0300

    Удалил проверку библиотеки thread.lua

--- applications/WirelessMSG.lua
+++ applications/WirelessMSG.lua
@@ -9,31 +9,18 @@ if(component.isAvailable("modem") == false or component.modem.isWireless() == fa
   print("У вас нету беспроводной сетевой карты!")
   return 0
 end
-if(require("filesystem").exists("lib/thread.lua") == false) then
-  print("Идёт установка библиотеки Thread...")
-  if(require("shell").execute("pastebin get E0SzJcCx /lib/thread.lua")) then
-    print("Успешно!")
-    os.sleep(2)
-    require("term").clear()
-  else
-    print("Ошибка! Возможно, вы не вставили интернет-плату или pastebin репозиторий удалён")
-    os.sleep(2)
-    require("term").clear()
-    return false
-  end
-end
 local thread = require("thread")
-thread.init()
 local modem = component.modem
 local cursorX, cursorY = 1, 1
 modem.open(port)
-modem.setStrength(3000)
+modem.setStrength(70000)
 print("Введите ваш ник: ")
 local yournick = require("term").read()
 tostring(yournick)
 require("term").clear()
 local function send(string)
   modem.broadcast(port,string.sub(yournick,0,string.len(yournick) - 1),"WirelessMSG_message",string)
+  require("computer").beep(1500)
 end
 local function wait()
   while true do
@@ -44,33 +31,41 @@ local function wait()
   send(tostring(msg))
   require("term").setCursor(1, cursorY)
   cursorY = cursorY + 1
+  if(cursorY == ySize) then
+    require("term").clear()
+    cursorY = 1
+    require("term").setCursor(1,cursorY)
+  end
   print(string.sub(yournick,0,string.len(yournick) - 1) .. "(вы)" .. ": " .. msg)
   require("term").setCursor(1, ySize)
   end  
 end
 local function waitMSG()
-while true do
-local act, receiver, sender, portReceive, distance, nick, type, msg = event.pull()
-  if(act == "modem_message") then
-    if(portReceive == port) then
-      if(type == "WirelessMSG_message") then
-        require("term").setCursor(1, cursorY)
-        cursorY = cursorY + 1
-        if(cursorY == ySize) then
-          require("term").clear()
-          cursorY = 1
+  while true do
+    local act, receiver, sender, portReceive, distance, nick, type, msg = event.pull()
+    if(act == "modem_message") then
+      if(portReceive == port) then
+        if(type == "WirelessMSG_message") then
+          require("term").setCursor(1, cursorY)
+          cursorY = cursorY + 1
+          if(cursorY == ySize) then
+            require("term").clear()
+            cursorY = 1
+            require("term").setCursor(1,cursorY)
+          end
+          print(nick .. "(" .. string.sub (sender, 0, 3) .. ")" .. ": " .. msg)
+          require("term").setCursor(1, ySize)
+          require("computer").beep(1500)
         end
-        print(nick .. "(" .. string.sub (sender, 0, 3) .. ")" .. ": " .. msg)
-        require("term").setCursor(1, ySize)
       end
     end
   end
-end
 end
 thread.create(waitMSG)
 thread.create(wait)
 while true do
 os.sleep(0.1)
 end
+
 
 
